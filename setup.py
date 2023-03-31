afcadefed9bb688b6657da1b082ee3480aba9c07
Germán Gasset <gassetgerman@gmail.com>
2023-03-31 14:17:41 +0200

    Finished setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,48 @@
+
+import os
 import numpy as np
 import pandas as pd
 
-global dataset_path, arms_imports, pivoted_arms_imports
 dataset_path = './Arms Imports Per Country (1950-2020).csv'
+cleaned_dataset_path = './ArmsImport.csv'
+original_arms_imports: pd.DataFrame
 arms_imports: pd.DataFrame
-pivoted_arms_imports: pd.DataFrame
+original_arms_imports = pd.read_csv(dataset_path)
+original_arms_imports.fillna(0, inplace=True)
 
 def main():
-    global arms_imports, pivoted_arms_imports
-    arms_imports = pd.read_csv(dataset_path)
-    arms_imports = arms_imports.fillna(0)
+    if os.path.isfile(cleaned_dataset_path) and __name__ != '__main__':
+        arms_imports = pd.read_csv(cleaned_dataset_path)
+        return 0
 
+    print(original_arms_imports.head())
+    organization_year_data = [year.__str__() for year in np.arange(1950, 2021, 1)] + ['Total']
+
+    year_data = organization_year_data * len(original_arms_imports['Country/Region/Group'])
+    organization_data: list[str]
+    organization_data = []
+    for organization in original_arms_imports['Country/Region/Group']:
+        organization_data += [organization] * len(organization_year_data)
+    values: list[float]
+    values = []
+    for organization in original_arms_imports['Country/Region/Group']:
+        organization_condition = original_arms_imports['Country/Region/Group'] == organization
+        organization_values = original_arms_imports[organization_condition][organization_year_data]
+        organization_values: pd.DataFrame
+        organization_values = list(organization_values.values[0])
+
+        values += organization_values
+        print(f'{len(values)}/{len(year_data)}')
+    df_data = {
+        'Country/Region/Group': organization_data,
+        'year': year_data,
+        'value': values
+        }
+
+    arms_imports = pd.DataFrame(data=df_data)
     print(arms_imports.head())
+    arms_imports.to_csv(cleaned_dataset_path)
 
-    columns_to_group = []
-    columns_to_group = ['Country/Region/Group'] + [v.__str__() for v in np.arange(1950, 2021, 1)]
-    print(columns_to_group)
-    #arms_imports.groupby()['Total']
+    return 0
 
-pivoted_df = main()
+main()
